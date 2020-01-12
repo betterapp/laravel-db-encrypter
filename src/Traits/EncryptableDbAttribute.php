82781612dd1d8d8c0e3de83490520d56d9b9bd5f
Daniel <biuro@betterapp.net>
2020-01-12 15:43:31 +0100

    cleanup

--- src/Traits/EncryptableDbAttribute.php
+++ src/Traits/EncryptableDbAttribute.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Crypt;
 
 /**
  * Trait EncryptableDbAttribute
- * @package BetterApp\LaravelDbEncrypter\Traits
+ * @package betterApp\LaravelDbEncrypter\Traits
  */
 trait EncryptableDbAttribute
 {
