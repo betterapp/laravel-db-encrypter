f5ec47de4aeb971a88ac807546a8b6ddfdfd4b25
daniel <biuro@beterapp.net>
2020-02-23 21:08:47 +0100

    allow decrypt before cast

--- src/Traits/EncryptableDbAttribute.php
+++ src/Traits/EncryptableDbAttribute.php
@@ -12,19 +12,43 @@ use Illuminate\Support\Facades\Crypt;
 trait EncryptableDbAttribute
 {
     /**
-     * @param string $key
+     * Get a plain attribute (not a relationship).
      *
+     * @param  string  $key
      * @return mixed
      */
-    public function getAttribute($key)
+    public function getAttributeValue($key)
     {
-        $value = parent::getAttribute($key);
+        $value = $this->getAttributeFromArray($key);
 
-        if (!in_array($key, $this->encryptable) || is_null($value) || $value === '') {
-            return $value;
+        // If the attribute has a get mutator, we will call that then return what
+        // it returns as the value, which is useful for transforming values on
+        // retrieval from the model to a form that is more useful for usage.
+        if ($this->hasGetMutator($key)) {
+            return $this->mutateAttribute($key, $value);
         }
 
-        return $this->decrypt($value);
+        // decrypt value before casts
+        if (in_array($key, $this->encryptable)) {
+            $value = $this->decrypt($value);
+        }
+
+        // If the attribute exists within the cast array, we will convert it to
+        // an appropriate native PHP type dependant upon the associated value
+        // given with the key in the pair. Dayle made this comment line up.
+        if ($this->hasCast($key)) {
+            return $this->castAttribute($key, $value);
+        }
+
+        // If the attribute is listed as a date, we will convert it to a DateTime
+        // instance on retrieval, which makes it quite convenient to work with
+        // date fields without having to create a mutator for each property.
+        if (in_array($key, $this->getDates()) &&
+            ! is_null($value)) {
+            return $this->asDateTime($value);
+        }
+
+        return $value;
     }
 
     /**
@@ -49,12 +73,43 @@ trait EncryptableDbAttribute
      */
     public function attributesToArray(): array
     {
-        $attributes = parent::attributesToArray();
+        // If an attribute is a date, we will cast it to a string after converting it
+        // to a DateTime / Carbon instance. This is so we will get some consistent
+        // formatting while accessing attributes vs. arraying / JSONing a model.
+        $attributes = $this->addDateAttributesToArray(
+            $attributes = $this->getArrayableAttributes()
+        );
 
-        if (empty($attributes)) {
-            return $attributes;
+        $attributes = $this->addMutatedAttributesToArray(
+            $attributes, $mutatedAttributes = $this->getMutatedAttributes()
+        );
+
+        // decrypt attributes before casts
+        $attributes = $this->decryptAttributes($attributes);
+
+        // Next we will handle any casts that have been setup for this model and cast
+        // the values to their appropriate type. If the attribute has a mutator we
+        // will not perform the cast on those attributes to avoid any confusion.
+        $attributes = $this->addCastAttributesToArray(
+            $attributes, $mutatedAttributes
+        );
+
+        // Here we will grab all of the appended, calculated attributes to this model
+        // as these attributes are not really in the attributes array, but are run
+        // when we need to array or JSON the model for convenience to the coder.
+        foreach ($this->getArrayableAppends() as $key) {
+            $attributes[$key] = $this->mutateAttributeForArray($key, null);
         }
 
+        return $attributes;
+    }
+
+    /**
+     * @param array $attributes
+     * @return array
+     */
+    private function decryptAttributes(array $attributes): array
+    {
         foreach ($attributes as $key => $value) {
             if (!in_array($key, $this->encryptable) || is_null($value) || $value === '') {
                 continue;
