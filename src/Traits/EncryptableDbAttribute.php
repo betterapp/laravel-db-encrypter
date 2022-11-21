b493c9294aff76b17fe72b53bcd2823c5611777e
betterapp <biuro@betterapp.net>
2022-11-21 17:27:56 +0100

    add support for Laravel 9

--- src/Traits/EncryptableDbAttribute.php
+++ src/Traits/EncryptableDbAttribute.php
@@ -12,30 +12,24 @@ use Illuminate\Support\Facades\Crypt;
  */
 trait EncryptableDbAttribute
 {
-    /**
-     * Get a plain attribute (not a relationship).
-     *
-     * @param  string  $key
-     * @return mixed
-     */
-    public function getAttributeValue($key)
+    protected function transformModelValue($key, $value)
     {
-        $value = $this->getAttributeFromArray($key);
-
         // If the attribute has a get mutator, we will call that then return what
         // it returns as the value, which is useful for transforming values on
         // retrieval from the model to a form that is more useful for usage.
         if ($this->hasGetMutator($key)) {
             return $this->mutateAttribute($key, $value);
+        } elseif ($this->hasAttributeGetMutator($key)) {
+            return $this->mutateAttributeMarkedAttribute($key, $value);
         }
 
         // decrypt value before casts
-        if (in_array($key, $this->encryptable) && !is_null($value) && $value !== '') {
+        if (!is_null($value) && $value !== '' && in_array($key, $this->encryptable)) {
             $value = $this->decrypt($value);
         }
 
         // If the attribute exists within the cast array, we will convert it to
-        // an appropriate native PHP type dependant upon the associated value
+        // an appropriate native PHP type dependent upon the associated value
         // given with the key in the pair. Dayle made this comment line up.
         if ($this->hasCast($key)) {
             return $this->castAttribute($key, $value);
@@ -44,8 +38,8 @@ trait EncryptableDbAttribute
         // If the attribute is listed as a date, we will convert it to a DateTime
         // instance on retrieval, which makes it quite convenient to work with
         // date fields without having to create a mutator for each property.
-        if (in_array($key, $this->getDates()) &&
-            ! is_null($value)) {
+        if ($value !== null
+            && \in_array($key, $this->getDates(), false)) {
             return $this->asDateTime($value);
         }
 
@@ -53,30 +47,72 @@ trait EncryptableDbAttribute
     }
 
     /**
-     * @param string $key
-     * @param mixed  $value
+     * Set a given attribute on the model.
      *
+     * @param  string  $key
+     * @param  mixed  $value
      * @return mixed
      */
     public function setAttribute($key, $value)
     {
-        if (is_null($value) || !in_array($key, $this->encryptable)) {
-            return parent::setAttribute($key, $value);
+        // First we will check for the presence of a mutator for the set operation
+        // which simply lets the developers tweak the attribute as it is set on
+        // this model, such as "json_encoding" a listing of data for storage.
+        if ($this->hasSetMutator($key)) {
+            return $this->setMutatedAttributeValue($key, $value);
+        } elseif ($this->hasAttributeSetMutator($key)) {
+            return $this->setAttributeMarkedMutatedAttributeValue($key, $value);
         }
 
-        if ($this->isJsonCastable($key) && !is_null($value)) {
+        // If an attribute is listed as a "date", we'll convert it from a DateTime
+        // instance into a form proper for storage on the database tables using
+        // the connection grammar's date format. We will auto set the values.
+        elseif (! is_null($value) && $this->isDateAttribute($key)) {
+            $value = $this->fromDateTime($value);
+        }
+
+        if ($this->isEnumCastable($key)) {
+            $this->setEnumCastableAttribute($key, $value);
+
+            return $this;
+        }
+
+        if ($this->isClassCastable($key)) {
+            $this->setClassCastableAttribute($key, $value);
+
+            return $this;
+        }
+
+        if (! is_null($value) && $this->isJsonCastable($key)) {
             $value = $this->castAttributeAsJson($key, $value);
         }
 
-        $value = $this->encrypt($value);
+        // If this attribute contains a JSON ->, we'll set the proper value in the
+        // attribute's underlying array. This takes care of properly nesting an
+        // attribute in the array's value in the case of deeply nested items.
+        if (str_contains($key, '->')) {
+            return $this->fillJsonAttribute($key, $value);
+        }
 
-        return parent::setAttribute($key, $value);
+        if (! is_null($value) && $this->isEncryptedCastable($key)) {
+            $value = $this->castAttributeAsEncryptedString($key, $value);
+        }
+
+        if (! is_null($value) && in_array($key, $this->encryptable)) {
+            $value = $this->encrypt($value);
+        }
+
+        $this->attributes[$key] = $value;
+
+        return $this;
     }
 
     /**
+     * Convert the model's attributes to an array.
+     *
      * @return array
      */
-    public function attributesToArray(): array
+    public function attributesToArray()
     {
         // If an attribute is a date, we will cast it to a string after converting it
         // to a DateTime / Carbon instance. This is so we will get some consistent
